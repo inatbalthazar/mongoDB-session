949b9e43b56552c257c348629218c492c9e2171b
inatbalthazar <inatbalthazar@gmail.com>
2026-07-06 23:14:44 +0800

    DoneExercise1-6

---
+++ query5.mongodb.js
@@ -0,0 +1,12 @@
+use("sample_mflix");
+
+// Exercise 3
+
+// How many theaters does AL state has?
+db.theaters.find({ "location.address.state": "AL" }).count();
+
+// How many theaters does La Quinta city has?
+db.theaters.find({ "location.address.city": "La Quinta" }).count();
+
+// What is an example of each documents of above like?
+db.theaters.findOne({ "location.address.city": "La Quinta" });
